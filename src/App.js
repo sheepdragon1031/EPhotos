0f3b0e20ea1d6577e55b6eecb0a7897b3582825d
Unknown <dragonsheep1031@gmail.com>
2019-05-24 16:39:57 +0800

    基本設定

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Index from './pages/index';
+import Appbar from './components/appbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const css = ({
+
+})
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Appbar />
+          <Router>
+            <Route exact path="/" component={Index} />
+          </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default withStyles(css)(App);
---
+++ src/components/appbar/index.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import pink from '@material-ui/core/colors/pink';
+import purple from '@material-ui/core/colors/purple';
+import grey from '@material-ui/core/colors/grey';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+      },
+    title:{
+        margin: 'auto',
+        background: grey[800],
+        borderRadius: '.25rem',
+    },
+    pink: {
+        fontWeight: 700,
+        color: pink[500],//pink 500
+        padding: '.25rem .5rem',
+    },
+    purple: {
+        background: purple[500],
+        color: grey[50],
+        padding: '.25rem .25rem',
+        borderRadius: '0 .25rem .25rem 0',
+    },
+    appbar:{
+        background: grey[300],
+    }
+});
+
+class App extends Component {
+  render() {
+    const { classes } = this.props
+    return (
+        <div className={classes.root}>
+            <AppBar className={classes.appbar} position="static" >
+                <Toolbar>
+                    {/* <IconButton edge="start" className={classes.menuButton} color="secondary" aria-label="Menu">
+                        <MenuIcon />
+                    </IconButton> */}
+                    <Typography variant="h6" className={classes.title}>
+                        <span className={classes.pink}>E</span>
+                        <span className={classes.purple}>Photos</span>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+  }
+}
+export default withStyles(styles)(App);
