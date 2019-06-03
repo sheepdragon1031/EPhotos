6110f919b7d9e9d4516987c6b7b48f11d1f81534
Unknown <dragonsheep1031@gmail.com>
2019-06-03 13:31:07 +0800

    相簿模式和拖拉

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,6 +4,13 @@ import FileBase64 from 'react-file-base64';
 import Gallery from 'react-photo-gallery';
 import Lightbox from 'react-images';
 import Dexie from 'dexie';
+import Dropzone from 'react-dropzone'
+import {SortableContainer, SortableElement} from 'react-sortable-hoc';
+import Photo from "./photo";
+import arrayMove from "array-move";
+import SelectedImage from "./selectedImage";
+
+
 const db = new Dexie('DexieDB');
 db.version(1).stores({
   DataSave: "++id, src, width, height, alt, keys",
@@ -20,6 +27,7 @@ const photos = [
   
 ];
 
+
 class index extends Component {
   
   constructor(props) {
@@ -28,10 +36,13 @@ class index extends Component {
     this.openLightbox = this.openLightbox.bind(this);
     this.gotoNext = this.gotoNext.bind(this);
     this.gotoPrevious = this.gotoPrevious.bind(this);
+    this.selectPhoto = this.selectPhoto.bind(this);
+    this.toggleSelect = this.toggleSelect.bind(this);
     this.state = {
       files: [],
       currentImage: 0,
       photo: [],
+      selectAll: false,
     }
     // db.DataSave.clear()
     db.DataSave.toArray().then(
@@ -44,8 +55,49 @@ class index extends Component {
     
    
   }
-  getFiles(files){
+  selectPhoto(event, obj) {
+    let photos = this.state.photos;
+    photos[obj.index].selected = !photos[obj.index].selected;
+    this.setState({ photos: photos });
+  }
+  toggleSelect() {
+    let photos = this.state.photos.map((photo, index) => {
+      return { ...photo, selected: !this.state.selectAll };
+    });
+    this.setState({ photos: photos, selectAll: !this.state.selectAll });
+  }
+  onSortEnd = ({ oldIndex, newIndex }) => {
+    if(oldIndex === newIndex){
+      this.openLightbox(newIndex)
+    }
+    else{
+      const oldIndexs =  this.state.photo[oldIndex]
+      const newIndexs =  this.state.photo[newIndex]
+
+      this.setState(({ photo }) => ({
+        photo: arrayMove(photo, oldIndex, newIndex)
+      }));
+
+      db.DataSave.update(oldIndexs.id,{
+        src: newIndexs.src ,
+        width: newIndexs.width,
+        height: newIndexs.height,
+        alt: newIndexs.alt,
+        keys: newIndexs.key,
+      })
+
+      db.DataSave.update(newIndexs.id,{
+        src: oldIndexs.src ,
+        width: oldIndexs.width,
+        height: oldIndexs.height,
+        alt: oldIndexs.alt,
+        keys: oldIndexs.key,
+      })
+    }
     
+
+  };
+  getFiles(files){
     files.map((index,value)=>{
       let img = new Image();
       img.src = index.base64;
@@ -73,9 +125,9 @@ class index extends Component {
     
   }
   
-  openLightbox(event, obj) {
+  openLightbox(index) {
     this.setState({
-      currentImage: obj.index,
+      currentImage: index,
       lightboxIsOpen: true,
     });
   }
@@ -95,22 +147,70 @@ class index extends Component {
       currentImage: this.state.currentImage + 1,
     });
   }
+  onDropHandler(files) { 
+    console.log('Drop');
+    files.map((index,value) =>{
+      console.log(index)
+      const reader = new FileReader();
+      const img = new Image();
+      reader.onload = (event) => {
+        console.log('reader');
+        index.base64 = event.target.result;
+        img.src = index.base64;
+        img.onload = () => {
+          console.log('img');
+          let content = {
+            src: index.base64 ,
+            width: img.width,
+            height: img.height,
+            alt: index.name,
+            keys: Date.now(),
+          }
+          db.DataSave.add(content);
+          db.DataSave.toArray().then(
+            (index)=>{
+              setTimeout(() => {
+                this.setState({photo: index})
+              });
+            })
+        }
+      };
+      reader.readAsDataURL(index);
+    })     
+  }
   render() {
     const { classes } = this.props
+    const SortablePhoto = SortableElement(item => <Photo {...item} />);
+    const SortableGallery = SortableContainer(({ items }) => (
+      <Gallery photos={items} renderImage={SortablePhoto}  />
+    ));
+
     return (
       <div className={classes.root}>
-        <Gallery photos={this.state.photo} onClick={this.openLightbox} />
         
-        <Lightbox images={this.state.photo}
-          onClose={this.closeLightbox}
-          onClickPrev={this.gotoPrevious}
-          onClickNext={this.gotoNext}
-          currentImage={this.state.currentImage}
-          isOpen={this.state.lightboxIsOpen}
-        />
-        <FileBase64
-          multiple={ true }
-          onDone={ this.getFiles.bind(this) } />
+        <Dropzone onDrop={ acceptedFiles => this.onDropHandler(acceptedFiles)}>
+          {({getRootProps, getInputProps}) => (
+            <section>
+              <div {...getRootProps()}>
+                
+                
+                <Lightbox images={this.state.photo}
+                  onClose={this.closeLightbox}
+                  onClickPrev={this.gotoPrevious}
+                  onClickNext={this.gotoNext}
+                  currentImage={this.state.currentImage}
+                  isOpen={this.state.lightboxIsOpen}
+                />
+                <FileBase64
+                  multiple={ true }
+                  onDone={ this.getFiles.bind(this) } />
+              
+                <input {...getInputProps()} />
+              </div>
+            </section>
+          )}
+        </Dropzone>
+        <SortableGallery items={this.state.photo} onSortEnd={this.onSortEnd}  axis={"xy"} />
       </div>
     )
   }
