6dd10a5950ac181eca8a59ba3e10f070dec27379
sheep <dragonsheep1031@gmail.com>
2019-06-14 03:37:21 +0800

    放棄useState

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,4 +1,4 @@
-import React, { Component , useState } from 'react';
+import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Gallery from 'react-photo-gallery';
 import Lightbox from 'react-images';
